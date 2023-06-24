bfc7218d27cf6e28f84e2fc69e1cf3c1bae6455a
Tan <Tan@user.com>
2023-06-24 11:49:52 +0800

    idea commit3

--- src/main/java/test/hello.java
+++ src/main/java/test/hello.java
@@ -4,6 +4,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello github!");
+        System.out.println("hello github2!");
         System.out.println("hello 远程 github!");
     }
 }
